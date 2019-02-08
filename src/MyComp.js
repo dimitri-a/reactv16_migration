3cf0c8e12bb338826797dbedb9d80ae46e83e52c
Dimitri C <couwenberg@gmail.com>
2019-02-08 21:39:38 +1100

    without state

--- src/MyComp.js
+++ src/MyComp.js
@@ -6,30 +6,36 @@ class MyComp extends Component {
 
     constructor(props) {
         super(props)
-        this.state = { result: MyComp.multiplication(1),hello:props.hier }
+       // this.state = { result: MyComp.multiplication(1),hello:props.hier }
         //debugger
     }
     componentDidMount() {
-        this.setState({ result: MyComp.multiplication(1) })
+       // this.setState({ result: MyComp.multiplication(1) })
     }
 
-    static getDerivedStateFromProps(nextProps, prevState) {
-        debugger
-        if (prevState.hello===nextProps.hier) return null
+    // static getDerivedStateFromProps(nextProps, prevState) {
+    //     debugger
+    //     if (prevState.hello===nextProps.hier) return null
 
-        return {
-          result: MyComp.multiplication(2),
-        };
-      }
+       
+    //   }
 
     static multiplication = (x) => {
         return x * 2
     }
 
+    renderMult(b){
+        if (b){
+            return <div>{MyComp.multiplication(2)}</div>
+        }
+        else
+        return  <div>{MyComp.multiplication(1)}</div>
+    }
+
     render() {
         return (
             <div>
-                {this.state.result}
+                {this.renderMult(this.props.hier)}
             </div>
         );
     }
